e34fa760b602616cc172b821debebe9480f0d71a
Emma-Havens <emmahavens2023@u.northwestern.edu>
2024-01-30 23:01:32 -0600

    Added Hand and Hand Items
    Hand is functional and attached to the player. Hand items have prefabs. Hand interaction tested, no item modification added (state change due to appliance).

---
+++ Assets/Code/Food_Item.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// all food items have one of the following states, according to how
+// it has been affected by appliances
+public enum State
+{
+    Raw,
+    Processed,
+    Ruined
+}
+
+// all food items is one of the following food types
+// must be given at item initialization
+public enum Food_type
+{
+    Burger,
+    Bun,
+    Lettuce,
+    Tomato,
+    Fries
+}
+
+public class Food_Item : Hand_Item
+{
+
+    public State state;      // either Raw, Processed, or Ruined
+    public Food_type type;   // either Burger, Bun, Lettuce, Tomato, Fries
+
+    void Start()
+    {
+        player_hand = FindObjectOfType<Hand>().GetComponent<Hand>();
+        item_collider = GetComponent<BoxCollider>();
+        item_renderer = GetComponent<MeshRenderer>();
+
+        state = State.Raw;
+        Debug.Log(state);
+        Debug.Log(type);
+    }
+
+    // appliances call this to cook or chop food items
+    void Process_food()
+    {
+        state = State.Processed;
+    }
+
+    // appliances call this to burn or ruin food items
+    void Ruin_food()
+    {
+        state = State.Ruined;
+    }
+}
---
+++ Assets/Code/Hand.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+
+    private Hand_Item item;     // player can only hold one thing at a time
+
+    void Start()
+    {
+        item = null;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    // sets the hand item to incoming object if hand is not full
+    public bool Pick_up_item(Hand_Item obj)
+    {
+        bool pick_up = false;
+        if (item == null)
+        {
+            item = obj;
+            pick_up = true;
+            Debug.Log("item has been picked up");
+        } else
+        {
+            Debug.Log("item has not been picked up");
+        }
+        return pick_up;
+    }
+
+    // returns the current hand item which MAY BE NULL
+    // the contents of the hand is 'used up'
+    public Hand_Item Use_item()
+    {
+        Hand_Item obj = null;
+        if (item != null)
+        {
+            obj = item;
+            item = null;
+            Debug.Log("item has been used");
+        } else
+        {
+            Debug.Log("no item currently in hand");
+        }
+        return obj;
+    }
+
+    // returns the current hand item WITHOUT using it up
+    public Hand_Item In_hand()
+    {
+        return item;
+    }
+
+}
