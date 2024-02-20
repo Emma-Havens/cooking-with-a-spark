f566763cb4de0028f0094c2ad5710f12cb88b2f6
Madeleine Carter <cmadeleine02@gmail.com>
2024-02-20 14:25:50 -0600

    progress bar edits

--- Assets/Code/Progress_Bar.cs
+++ Assets/Code/Progress_Bar.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Progress_Bar : MonoBehaviour
 {
     public GameObject bar;
     public GameObject full_bar;
+    public TextMeshPro text;
     Cooking_appliance appliance;
     private int processed;
     private int ruined;
@@ -30,27 +32,39 @@ public class Progress_Bar : MonoBehaviour
        bar.transform.localScale = new Vector3(0, 0, 0);
        bar.SetActive(e);
        full_bar.SetActive(e);
+       text.text = "";
 
     }
 
     public void SetProgress(float prog)
     {
-        float val = prog / (processed + 200);
+        float val = prog / processed;
+        if (val > 1) val = 1;
 
         bar.transform.localScale = new Vector3(val , 1, 1);
         bar.transform.localPosition = new Vector3 ((1.0f-val) * 5.0f, 0.01f, 0.0f);
-;
-        if (prog < 3.0f * processed/4 | prog > ruined)
+
+        if (prog < processed / 2.0f)
         {
-            bar.GetComponent<MeshRenderer>().material.color = new Color(1, 0, 0);   
+            bar.GetComponent<MeshRenderer>().material.color = new Color(1 + 0.2f, val * 2 - 0.2f, 0);
+            text.text = (Mathf.RoundToInt(val * 100)).ToString() + "%";
         }
         else if (prog < processed)
         {
-            bar.GetComponent<MeshRenderer>().material.color = new Color(1 - (4 * prog / processed - 3), (4 * prog / processed) - 3, 0);
+            bar.GetComponent<MeshRenderer>().material.color = new Color((1 - val) * 2 + 0.2f, 1 - 0.2f, 0);
+            text.text = (Mathf.RoundToInt(val * 100)).ToString() + "%";
+        }
+        else if (prog < ruined)
+        {
+            bar.GetComponent<MeshRenderer>().material.color = Color.green;
+            text.text = "TAKE OUT";
         }
         else
         {
-            bar.GetComponent<MeshRenderer>().material.color = new Color(0, 1, 0);
+            bar.GetComponent<MeshRenderer>().material.color = Color.red;
+            text.text = "RUINED";
         }
+
+        
     }
 }
