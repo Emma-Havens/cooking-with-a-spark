f76670f24a5b8133024eec6293f7e2e79d27ce7f
Josh Matkowsky <joshmatkowsky@gmail.com>
2024-03-05 03:14:58 -0600

    Electrical rework
    switch near each appliance
    breaker now acts as emergency shutoff
    electricity load display

--- Assets/Code/Breaker.cs
+++ Assets/Code/Breaker.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Breaker : MonoBehaviour
+public class Breaker : Interactable
 {
     public AudioClip flip_breaker;
     public AudioClip breaker_error;
@@ -12,9 +12,14 @@ public class Breaker : MonoBehaviour
     public int max_load = 3;
     int current_load = 0;
 
+    LoadKeeper loadKeeper;
+
     private void Start()
     {
         audio_s = GetComponent<AudioSource>();
+        GetComponent<Renderer>().material.color = Color.red;
+
+        loadKeeper = FindObjectOfType<LoadKeeper>().GetComponent<LoadKeeper>();
     }
 
     //called by switches
@@ -34,7 +39,20 @@ public class Breaker : MonoBehaviour
             Debug.Log("Load increased");
             current_load++;
             audio_s.PlayOneShot(flip_breaker);
+
+            loadKeeper.Increase();
             return true;
+        }
+    }
+
+    public override void Interact()
+    {
+        foreach (var breaker_switch in FindObjectsOfType<Breaker_Switch>())
+        {
+            if (breaker_switch.on)
+            {
+                breaker_switch.TurnOff();
+            }
         }
     }
 
@@ -43,6 +61,8 @@ public class Breaker : MonoBehaviour
         Debug.Log("Load decreased");
         current_load--;
         audio_s.PlayOneShot(flip_breaker);
+
+        loadKeeper.Decrease();
     }
 
     public int get_current_load()
--- Assets/Code/Breaker_Switch.cs
+++ Assets/Code/Breaker_Switch.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class Breaker_Switch : Interactable
 {
 
-    //has to be manually set for each switch in the inspector
-    public Cooking_appliance appliance;
-    bool on = false;
-
+    Cooking_appliance appliance;
     Breaker breaker;
+    
     Renderer ren;
+
+    public bool on = false;
 
     private void Start()
     {
         breaker = FindObjectOfType<Breaker>().GetComponent<Breaker>();
+        appliance = transform.parent.GetComponent<Cooking_appliance>();
 
         ren = GetComponent<Renderer>();
         ren.material.color = Color.red;
+
+        transform.Rotate(20, 0, 0);
     }
 
     public override void Interact()
@@ -35,23 +38,29 @@ public class Breaker_Switch : Interactable
 
         else
         {
-            breaker.remove_load();
-            appliance.is_powered = false;
-
-            AnimateOff();
-            on = false;
+            TurnOff();
         }
     }
+
+    public void TurnOff()
+    {
+        breaker.remove_load();
+        appliance.is_powered = false;
+
+        AnimateOff();
+        on = false;
+    }
+
 
     private void AnimateOn()
     {
         ren.material.color = Color.green;
-        transform.rotation = Quaternion.Euler(-10, 0, 0);
+        transform.Rotate(Vector3.left * 50);
     }
 
     private void AnimateOff()
     {
         ren.material.color = Color.red;
-        transform.rotation = Quaternion.Euler(10, 0, 0);
+        transform.Rotate(Vector3.right * 50);
     }
 }
