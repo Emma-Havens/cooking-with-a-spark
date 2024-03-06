718e50110f712cb9b93909d898901949995ef3aa
macapay <ethanmcalpin2024@u.northwestern.edu>
2024-03-06 17:15:37 -0600

    final changes
    made building windows executable work, also shrank electrical load sign

--- Assets/Code/Assembly_Station.cs
+++ Assets/Code/Assembly_Station.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.Timeline.Actions.MenuPriority;
+//using static UnityEditor.Timeline.Actions.MenuPriority;
 using TMPro;
 
 public class Assembly_Station : Counter
--- Assets/Code/Raycast.cs
+++ Assets/Code/Raycast.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using static UnityEditor.Progress;
+//using static UnityEditor.Progress;
 
 public class Raycast : MonoBehaviour
 {
